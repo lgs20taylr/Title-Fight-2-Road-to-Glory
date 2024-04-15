aedd50f40acec70e3792a289639a68fd4f85a8d1
lgs20taylr <152493212+lgs20taylr@users.noreply.github.com>
2024-04-15 19:32:36 +0100

    a

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ class player():
         self.age = age
     
     def load():
+        print("a")
 
 connection = sqlite3.connect("rtgdatabase.db")
 
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS players (id INTEGER,firstName TEXT,lastName TEXT,age INTEGER)")
+cursor.execute("CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY,firstName TEXT,lastName TEXT,age INTEGER)")
 
-cursor.execute("INSERT INTO players VALUES (0, 'Lionel','Messi',36)")
+cursor.execute("INSERT INTO players (firstName,lastName,age) VALUES ('Lionel','Messi',36)")
 
-cursor.execute("SELECT * FROM players WHERE id = 0")
+cursor.execute("SELECT * FROM players WHERE id = 1")
 
 a = cursor.fetchall()
 print(a)
